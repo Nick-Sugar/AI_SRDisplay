9685f5ca7834f9d1cc3dd763e2e1d37fbe32ecff
UserIsDevious <user.devious@gmail.com>
2020-11-18 02:10:09 +0900

    test

---
+++ SDR_Test/Assets/TalkSystem/GetResponse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetResponse : MonoBehaviour
+{
+    string url = "https://script.google.com/macros/s/AKfycbyD0yruRnfUiIGUVdXPboTFCjr2uvkKGIZrwhXdpPEClJROiSAN/exec?text=日本";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetStart());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator GetStart()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+
+        yield return request.SendWebRequest();
+
+        string JSONstring = request.downloadHandler.text;
+        Debug.Log(JSONstring);
+    }
+}
